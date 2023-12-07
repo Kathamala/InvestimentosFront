9a890ba9c9a996c46aa37d5256c06a721309bed1
Thiago Vinícius Azevedo de Oliveira <thiago.vinicius@smartsuite.imd.ufrn.br>
2023-12-06 21:35:14 -0300

    Add dependencies and pie chart component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PieChartComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PieChartComponent from './components/PieChartComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PieChartComponent
   }
 }
 </script>
