51355b35296598d7d975b34b550b6e392107bbf5
josevitoor <josevitor_12@live.com>
2023-12-12 19:38:26 -0300

    ajuste de identação do form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import "./App.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import WalletList from "./page/WalletList";
 import { Card, Button } from "antd";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import WalletForm from "./page/WalletForm";
-/*
-const data1 = [
-  ["Task", "Hours per Day"],
-  ["Work", 11],
-  ["Eat", 2],
-  ["Commute", 2],
-  ["Watch TV", 2],
-  ["Sleep", 7],
-];
 
-const data2 = [
-  ["Task", "Hours per Day"],
-  ["Work", 8],
-  ["Eat", 3],
-  ["Commute", 2],
-  ["Watch TV", 4],
-  ["Sleep", 7],
-];
-*/
 function App() {
   const [wallets, setWallets] = useState([]);
 
@@ -32,35 +14,36 @@ function App() {
   }, []);
 
   const fetchData = () => {
-    axios.get('http://localhost:8080/api/wallet/getAll')
-    .then(response => {
-      setWallets(response.data);
-    })
-    .catch(error => {
-      console.error('Error fetching data: ', error);
-    });
+    axios
+      .get("http://localhost:8080/api/wallet/getAll")
+      .then((response) => {
+        setWallets(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
   };
 
   return (
     <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={      
-            <Card title="My Wallets">
-              <Link to="/new-wallet">
-                <Button type="primary">
-                  New Wallet
-                </Button>
-              </Link>
-              <WalletList wallets={wallets} />
-            </Card>
-          }/>
-          <Route path="new-wallet" element={<WalletForm />} />
-        </Route>
-      </Routes>
-    </BrowserRouter> 
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <Card title="Minhas Carteiras">
+                  <Link to="/new-wallet">
+                    <Button type="primary">Nova Carteira</Button>
+                  </Link>
+                  <WalletList wallets={wallets} />
+                </Card>
+              }
+            />
+            <Route path="new-wallet" element={<WalletForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/page/WalletForm.js
+++ src/page/WalletForm.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-import { Form, Input, Select, Button, Row, Col, Space, message } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import { Form, Input, Select, Button, Row, Col, message, Divider } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import { Card } from "antd";
+import axios from "axios";
 
 const WalletForm = (props) => {
   const [form] = Form.useForm();
   const [investimentList, setInvestimentList] = useState([
     {
-      name: '',
+      name: "",
       yieldRate: null,
       value: null,
       type: null,
     },
   ]);
   const [, setInvestimentGoal] = useState(null);
-  
+
   const { Option } = Select;
 
   const addInvestimento = () => {
     setInvestimentList([
       ...investimentList,
       {
-        name: '',
+        name: "",
         yieldRate: null,
         value: null,
         type: null,
@@ -39,7 +40,7 @@ const WalletForm = (props) => {
   const clear = () => {
     setInvestimentList([
       {
-        name: '',
+        name: "",
         yieldRate: null,
         value: null,
         type: null,
@@ -53,148 +54,172 @@ const WalletForm = (props) => {
     form
       .validateFields()
       .then((values) => {
-        if(values.investimentList === undefined || values.investimentList.size === 0){
-            message.error('Insert at least one investiment!');
-            return;
+        if (
+          values.investimentList === undefined ||
+          values.investimentList.size === 0
+        ) {
+          message.error("Insert at least one investiment!");
+          return;
         }
-        var postData = {}
-        postData["firstYearInvestments"] = values.investimentList
-        postData["goal"] = values.investimentGoal
-        axios.post('http://localhost:8080/api/wallet', postData)
-        .then(response => {
+        var postData = {};
+        postData["firstYearInvestments"] = values.investimentList;
+        postData["goal"] = values.investimentGoal;
+        axios
+          .post("http://localhost:8080/api/wallet", postData)
+          .then((response) => {
             console.log(response.data);
-            window.location.href = '/'
-        })
-        .catch(error => {
-            console.error('Error fetching data: ', error);
-        });
+            window.location.href = "/";
+          })
+          .catch((error) => {
+            console.error("Error fetching data: ", error);
+          });
       })
       .catch((error) => {
-        console.error('Validation failed:', error);
+        console.error("Validation failed:", error);
       });
   };
 
   return (
-    <div style={{ width: '90%', margin: '0 auto' }}>
-      <h1>New Wallet</h1>
-      <Form form={form} onFinish={submitForm}>
-        {investimentList.map((field, index) => (
-          <div key={index}>
-            <Row gutter={16} justify="space-between" align="middle">
-              <Col span={6}>
-                <Form.Item
-                  name={['investimentList', index, 'name']}
-                  rules={[{ required: true, message: 'Informe um nome!' }]}
-                >
-                <Input
-                    placeholder="Nome"
-                    type="text"
-                />
-                </Form.Item>
-              </Col>
-              <Col span={6}>
-                <Form.Item
-                  name={['investimentList', index, 'yieldRate']}
-                  rules={[
-                    { required: true, message: 'Informe uma taxa de rendimento!' },
-                    {
-                      pattern: /^-?\d*(\.\d*)?$/,
-                      message: 'Informe um valor numérico!',
-                    },
-                  ]}
-                >
-                <Input
-                    addonAfter="%"
-                    placeholder="Taxa de Rendimento"
-                    type="number"
-                    step="1"
-                />
-                </Form.Item>
-              </Col>
-              <Col span={6}>
-                <Form.Item
-                  name={['investimentList', index, 'type']}
-                  rules={[{ required: true, message: 'Informe um tipo de investimento!' }]}
-                >
-                  <Select
-                        placeholder="Tipo de Investimento"
-                    >
-                    <Option value="Renda Fixa">Renda Fixa</Option>
-                    <Option value="Renda Variável">Renda Variável</Option>
-                    <Option value="Imóvel">Imóvel</Option>
-                    <Option value="Automóvel">Automóvel</Option>
-                  </Select>
-                </Form.Item>
-              </Col>
-              <Col span={6}>
-                <Form.Item
-                  name={['investimentList', index, 'value']}
-                  rules={[
-                    { required: true, message: 'Informe um valor!' },
-                    {
+    <>
+      <Card title="Minha Carteira">
+        <Form form={form} onFinish={submitForm} layout="vertical">
+          <Button type="primary" onClick={addInvestimento}>
+            + Adicionar Investimento
+          </Button>
+          <Row gutter={16} style={{ marginTop: 20 }}>
+            {investimentList.map((field, index) => (
+              <>
+                <Col span={6}>
+                  <Form.Item
+                    label="Nome"
+                    name={["investimentList", index, "name"]}
+                    rules={[{ required: true, message: "Informe um nome!" }]}
+                  >
+                    <Input placeholder="Nome" type="text" />
+                  </Form.Item>
+                </Col>
+                <Col span={4}>
+                  <Form.Item
+                    label="Taxa de Rendimento"
+                    name={["investimentList", index, "yieldRate"]}
+                    rules={[
+                      {
+                        required: true,
+                        message: "Informe uma taxa de rendimento!",
+                      },
+                      {
                         pattern: /^-?\d*(\.\d*)?$/,
-                        message: 'Informe um valor numérico!',
-                    }
-                ]}
-                >
-                <Input
-                    addonBefore="R$"
-                    placeholder="Valor do Investimento"
-                    type="number"
-                    step="1"
-                />
-                </Form.Item>
-              </Col>
-              <Col>
-                <Space>
+                        message: "Informe um valor numérico!",
+                      },
+                    ]}
+                  >
+                    <Input
+                      addonAfter="%"
+                      placeholder="Taxa de Rendimento"
+                      type="number"
+                      step="1"
+                    />
+                  </Form.Item>
+                </Col>
+                <Col span={6}>
+                  <Form.Item
+                    label="Tipo de Investimento"
+                    name={["investimentList", index, "type"]}
+                    rules={[
+                      {
+                        required: true,
+                        message: "Informe um tipo de investimento!",
+                      },
+                    ]}
+                  >
+                    <Select placeholder="Tipo de Investimento">
+                      <Option value="Renda Fixa">Renda Fixa</Option>
+                      <Option value="Renda Variável">Renda Variável</Option>
+                      <Option value="Imóvel">Imóvel</Option>
+                      <Option value="Automóvel">Automóvel</Option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+                <Col span={6}>
+                  <Form.Item
+                    label="Valor do Investimento"
+                    name={["investimentList", index, "value"]}
+                    rules={[
+                      { required: true, message: "Informe um valor!" },
+                      {
+                        pattern: /^-?\d*(\.\d*)?$/,
+                        message: "Informe um valor numérico!",
+                      },
+                    ]}
+                  >
+                    <Input
+                      addonBefore="R$"
+                      placeholder="Valor do Investimento"
+                      type="number"
+                      step="1"
+                    />
+                  </Form.Item>
+                </Col>
+                <Col span={2}>
                   <Button
-                    type="text"
+                    danger
+                    style={{ marginTop: 30, marginLeft: 60 }}
                     onClick={() => removeInvestimento(index)}
                     icon={<DeleteOutlined />}
-                  />
-                </Space>
-              </Col>
-            </Row>
-          </div>
-        ))}
-        <Button type="primary" onClick={addInvestimento}>
-          + Adicionar Investimento
-        </Button>
-        <Row gutter={16}>
-          <Col span={12}>
-            <Form.Item
-              name="investimentGoal"
-              label="Meta de Investimento"
-              rules={[
-                { required: true, message: 'Informe uma meta de investimento!' },
-                {
-                  pattern: /^-?\d*(\.\d*)?$/,
-                  message: 'Informe um valor numérico!',
-                },
-              ]}
-            >
+                  >
+                    Delete
+                  </Button>
+                </Col>
+              </>
+            ))}
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="investimentGoal"
+                label="Meta de Investimento"
+                rules={[
+                  {
+                    required: true,
+                    message: "Informe uma meta de investimento!",
+                  },
+                  {
+                    pattern: /^-?\d*(\.\d*)?$/,
+                    message: "Informe um valor numérico!",
+                  },
+                ]}
+              >
                 <Input
-                    addonBefore="R$"
-                    placeholder="Meta de Investimento"
-                    type="number"
-                    step="100"
+                  addonBefore="R$"
+                  placeholder="Meta de Investimento"
+                  type="number"
+                  step="100"
                 />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button type="primary" onClick={clear}>
-          Clear
-        </Button>
-    </Form>
-    <Link to="/">
-        <Button type="primary">
-            Cancel
-        </Button>
-    </Link>
-    </div>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Card>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: 25,
+        }}
+      >
+        <Link to="/">
+          <Button type="default">Cancelar</Button>
+        </Link>
+        <div>
+          <Button type="default" onClick={clear} style={{ marginRight: 8 }}>
+            Limpar
+          </Button>
+          <Button type="primary" htmlType="submit">
+            Salvar
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
 
